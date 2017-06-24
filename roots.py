1336f3267c6c0e881bc590f40fe2aafe6189e2e1
calebseagren <cseagre@siue.edu>
2017-06-24 13:27:22 -0500

    Update roots.py

--- roots.py
+++ roots.py
@@ -1,1 +1,18 @@
 #caleb Seagren
+import math
+
+a = int(input("Enter the coefficient of a: "))
+b = int(input('Enter the coefficient of b: '))
+c = int(input('Enter the coefficient of c: '))
+
+d=b**2-(4*a*c)
+print d
+if d<0:
+	print 'Error: this has complex roots'
+elif d == 0:
+	x1=(-b+math.sqrt((b**2)-(4*a*c)))/(2*a)
+	print "This equation has only one solution: ", x1
+else:
+	x1=(-b+math.sqrt((b**2)-(4*a*c)))/(2*a)
+	x2=(-b-math.sqrt((b**2)-(4*a*c)))/(2*a)
+	print 'The solution to this equation is ',x1, 'or', x2
